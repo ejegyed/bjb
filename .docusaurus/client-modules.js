97c275ce228d112f278d5773105a5397877b4426
EJ Egyed <>
2025-12-02 11:59:18 -0500

    Switch to docusaurus

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("/Users/ej/git/bjb/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
+  require("/Users/ej/git/bjb/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/ej/git/bjb/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/ej/git/bjb/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/ej/git/bjb/src/css/custom.css"),
+];
